d89a5e4cea1b7ba54ff65c10a6e12aeb151c62e7
Eraser <eraser1981@gmail.com>
2026-04-17 09:31:17 +0300

    columns view objects

--- tree_cache.py
+++ tree_cache.py
@@ -125,7 +125,10 @@ def attach_object_templates(objects, render_templates=False):
         obj_dict = obj.copy()
         if render_templates:
             runtime_object = getObject(obj_dict["name"])
-            obj_dict["template"] = runtime_object.render() if runtime_object else ""
+            if runtime_object and runtime_object.has_render_template():
+                obj_dict["template"] = runtime_object.render()
+            else:
+                obj_dict["template"] = ""
         else:
             obj_dict["template"] = ""
         hydrated_objects.append(obj_dict)
